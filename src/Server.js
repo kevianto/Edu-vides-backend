99ef37bf73e49115ddfac19042e817ecac498ad5
kevianto <kevinshahale746@gmail.com>
2025-03-23 13:07:16 +0300

    ogdh

--- src/Server.js
+++ src/Server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "https://eduvibesvideos.vercel.app/", 
+  origin: "http://localhost:5173/", 
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization",
 }));
