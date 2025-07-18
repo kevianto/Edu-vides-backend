155fea2b1348eeb5206e78f0f128fa6ccd82ccfa
kevianto <kevinshahale746@gmail.com>
2025-07-18 12:19:28 +0300

    updating login controller

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -20,6 +20,11 @@ const login = async (req, res) => {
     return res.status(200).json({
       success: true,
       message: "Login successful",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
       token,
     });
   } catch (error) {
